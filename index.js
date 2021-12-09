debd034be8069bad2f75df2a0c8e671d20442129
Tri Doan <tridoan1998@gmail.com>
2021-12-09 09:54:48 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+
+    state = {lat: null, errorMessage: ''};
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat: position.coords.latitude}),
+            err => this.setState({errotMessage: err.message})
+            );
+    }
+    
+
+    renderContent() {
+        if (this.state.errorMessage && !this.state.lat){
+            return <div> Error {this.state.errorMessage} </div>;
+
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat}/>
+        }
+
+        return <Spinner message="Please accept location request"/>;
+
+    }
+    //react need render!
+    render(){
+        return (
+            <div className="border ed">
+                {this.renderContent()}
+            </div>
+        )
+
+        }
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+
+);
