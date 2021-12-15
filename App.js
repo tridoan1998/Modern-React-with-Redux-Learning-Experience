ca84f9115cc2f3f4b986be7960309ed78f23b7fa
Tri Doan <tridoan1998@gmail.com>
2021-12-14 23:41:41 -0800

    Add files via upload

--- App.js
+++ App.js
@@ -1,30 +1,55 @@
 import React from 'react';
-import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
-import ImageList from './ImageList';
+import youtube from '../apis/youtube';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
-class App extends React.Component {
-    state = { images: [] };
+class App extends React.Component{
+    state = {videos: [], selectedVideo: null};
 
-    
-    onSearchSubmit = async (term) => {
-        const response = await unsplash.get('https://api.unsplash.com/search/photos', {
-            params: { query: term}
-        });
-
-        this.setState({images: response.data.results});
+    componentDidMount() {
+        this.onTermSubmit('korean songs');
 
     }
 
+    onTermSubmit = async term => {
+    const response = await youtube.get('/search', {
+        params: {
+            q: term
+        }
+    });
+
+    this.setState({
+        videos: response.data.items,
+        selectedVideo: response.data.items[0]
+    
+    });
+};
+
+    onVideoSelect = video => {
+        this.setState({selectedVideo: video});
+    }
 
     render() {
-    return(
-    <div className="ui container" style={{marginTop: '10px'}}>
-        <SearchBar onSubmit={this.onSearchSubmit}/>
-        <ImageList images ={this.state.images}/>
-        </div>
-    );
+        return( 
+            <div className = "ui container">
+            <SearchBar onFormSubmit = {this.onTermSubmit}/>
+                <div className = "ui grid">
+                    <div className = "ui row">
+                        <div className = "eleven wide column">
+                            <VideoDetail video = {this.state.selectedVideo} />
+                        </div>
+                        <div className = "five wide column">
+                             <VideoList 
+                                onVideoSelect= {this.onVideoSelect} 
+                                videos={this.state.videos}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
     }
-};
+}
 
 export default App;
--- SearchBar.js
+++ SearchBar.js
@@ -1,30 +1,38 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 class SearchBar extends React.Component {
     state = {term: ''};
-    
+
+
+    onInputChange = event => {
+        this.setState({term: event.target.value});
+    };
+
     onFormSubmit = event => {
         event.preventDefault();
-
-        this.props.onSubmit(this.state.term);
+        
+        this.props.onFormSubmit(this.state.term);
     };
 
     render() {
-        return(
-        <div className="ui segment"> 
-        <form onSubmit={this.onFormSubmit} className= "ui form">
-            <div className="field">
-                <label> Image Search</label>
-            <input type="text" 
-            value= {this.state.term}
-            onChange={e=> this.setState({term: e.target.value})}/>   
+        return (
+            <div className="search-bar ui segment">
+                <form onSubmit={this.onFormSubmit} className="ui form">
+                    <div className="field">
+                        <label> Video Search</label>
+                        <input 
+                            type="text"
+                            value= {this.state.term}
+                            onChange = {this.onInputChange}
+                            /> 
+                </div>
+            </form>
             </div>
-        </form>    
 
-        </div>
         );
+
     }
+
 }
 
 export default SearchBar;
